fbae4edf6db623c3831865bc98b1740326d35f41
0sik <ghdtlrdudtlr@naver.com>
2022-07-17 22:53:27 +0900

    comparison

---
+++ javaScript/comparison.js
@@ -0,0 +1,23 @@
+console.log(7===3); // 7은 3과 같은가? => false
+console.log(7!==3); // 7은 3과 다른가? => true
+
+console.log(7>3); //7은 3보다 큰가? = true
+console.log(7>=3); //7은 3보다 큰거나 같은가? =true
+
+console.log(7<3); //7은 3보다 작은가? =false
+console.log(7<=3); //7은 3보다 작거나 큰가? =false
+
+//boolean 연산자
+//AND
+console.log(true&&true); //true
+console.log(true&&false); //false
+console.log(false&&true); //false
+console.log(false&&false); //false
+//OR
+console.log(true || ture); //true
+console.log(true || false); //true
+console.log(false || ture); //true
+console.log(false || false); //false
+//NOT(!)
+console.log(!true) // false
+console.log(!false) // true
